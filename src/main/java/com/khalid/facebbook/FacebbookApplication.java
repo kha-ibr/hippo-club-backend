381ab8aa0febf8efd886514cf128b5d32f8aecbe
Khalid Ibrahim <khaibr-19@outlook.com>
2021-05-03 19:24:34 +0200

    first commit

---
+++ src/main/java/com/khalid/facebbook/FacebbookApplication.java
@@ -0,0 +1,13 @@
+package com.khalid.facebbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FacebbookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FacebbookApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/khalid/facebbook/FacebbookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.khalid.facebbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FacebbookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
