d4c37d0158a2b59a80d017fe8fb7846877e143c4
Khalid Ibrahim <64450080+khaibr20@users.noreply.github.com>
2021-06-01 12:42:09 +0200

    feat: add security config

---
+++ src/main/java/com/khalid/hippoClub/Config/SecurityConfig.java
@@ -0,0 +1,16 @@
+package com.khalid.hippoClub.Config;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    public void configure(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.cors().and()
+                .csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/api/auth/**")
+                .permitAll();
+    }
+}
